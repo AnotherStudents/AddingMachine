77d2a82ddd09578382038ca7bc217efd2bb68c6b
chadc <chadc@DESKTOP-2MK3LAL.mshome.net>
2020-10-07 19:21:51 -0700

    Added the code to the program

--- AddingMachineFix/src/cse360assignment02/AddingMachine.java
+++ AddingMachineFix/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,32 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "0 ";
   //Constructor
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+	  total = 0;  // not needed - included for clarity
   }
   //returns the total
   public int getTotal () {
-    return 0;
+	  return total;
   }
   //adds the number passed to the total
   public void add (int value) {
+	  total += value;
+	  history += "+ " + value + " ";
   }
   //subtracts the number passed from the total
   public void subtract (int value) {
+	  total = total - value;
+	  history += "- " + value + " ";
   }
   //returns a toString of the "history" of the use of the programs methods
   public String toString () {
-    return "";
+	  return history;
   }
   //clears the "history" basically refreshing the class
   public void clear() {
+	  history = "0";
+	  total = 0;
   }
 }
