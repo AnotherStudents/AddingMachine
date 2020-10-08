845775dccf7b00a6336e0872194d118f3958ec6c
chadc <chadc@DESKTOP-2MK3LAL.mshome.net>
2020-10-07 19:06:56 -0700

    Initial

---
+++ AddingMachineFix/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  //Constructor
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  //returns the total
+  public int getTotal () {
+    return 0;
+  }
+  //adds the number passed to the total
+  public void add (int value) {
+  }
+  //subtracts the number passed from the total
+  public void subtract (int value) {
+  }
+  //returns a toString of the "history" of the use of the programs methods
+  public String toString () {
+    return "";
+  }
+  //clears the "history" basically refreshing the class
+  public void clear() {
+  }
+}
